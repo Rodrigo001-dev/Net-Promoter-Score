f91f4a676a541dc86c97f25d52f8290cb91b968b
Rodrigo <rodrigorael53@gmail.com>
2021-02-23 10:56:39 -0300

    configure the migration directory

---
+++ back-end/src/database/migrations/1614088474242-create_users.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createUsers1614088474242 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
