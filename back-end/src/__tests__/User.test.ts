9459a4040bbfc10eef3419a2f751bde18d8d18b8
Rodrigo <rodrigorael53@gmail.com>
2021-02-24 12:24:22 -0300

    separating server in another file to use for testing

---
+++ back-end/src/__tests__/User.test.ts
@@ -0,0 +1,22 @@
+
+// vai descrever o que ser feito
+// describe("First", () => {
+  
+  // para criar um teste é utilizando a função it()
+  // no primeiro parâmetro é descripto o teste com o máximo de detalhes
+  // possível para quando rodar os testes só olhar a descrição e saber o que
+  // o teste está fazendo
+  // o segundo parâmetro é uma função
+  // it("deve somar 2 numeros", () => {
+    // dentro do teste tem 2 informações o expect => o que é esperado, pode ser
+    // uma função, um serviço que está sendo criado entre outras coisa
+
+    // toBe => que seja, que me retorne
+    // expect(2 + 2).toBe(4);
+  // });
+
+  // it("deve somar 2 numeros", () => {
+    // no.toBe => espera que 2 + 2 não seja 5
+    // expect(2 + 2).not.toBe(5);
+  // });
+// });
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,12 +1,3 @@
-import './database';
-import 'reflect-metadata';
-
-import express from 'express';
-import { router } from './routes';
-
-const app = express();
-
-app.use(express.json());
-app.use(router);
+import { app } from './app';
 
 app.listen(process.env.PORT || 3333);
