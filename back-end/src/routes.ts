ea7ef65ab88f43254b33d8ff0544c45c2f47cfeb
Rodrigo <rodrigorael53@gmail.com>
2021-02-24 11:47:37 -0300

    add route list to survey

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import createUser from './controllers/UserController/create';
 
 import createSurvey from './controllers/SurveysController/create';
+import ListSurvey from './controllers/SurveysController/index';
 
 const router = Router();
 
 router.post("/users", createUser);
 
 router.post("/surveys", createSurvey);
+router.get("/surveys", ListSurvey);
 
 export { router };
