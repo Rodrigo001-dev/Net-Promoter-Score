31d1e6b0203787baa539465c96ffae0188445605
Rodrigo <rodrigorael53@gmail.com>
2021-02-24 14:22:50 -0300

    doing first test user creation

--- back-end/src/__tests__/User.test.ts
+++ back-end/src/__tests__/User.test.ts
@@ -26,8 +26,27 @@ import createConnection from '../database';
 // });
 
 describe("User", () => {
-  request(app).post("/users").send({
-    email: "user@example.com",
-    name: "User Example"
+  // beforeAll => antes de tudo vai executar as migrations
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+  
+  it("Should be able to create a new user", async () => {
+    const response = await request(app).post("/users").send({
+      email: "user@example.com",
+      name: "User Example"
+    });
+
+    expect(response.status).toBe(201);
+  });
+
+  it("Should not be able to create a new user with exists email", async () => {
+    const response = await request(app).post("/users").send({
+      email: "user@example.com",
+      name: "User Example"
+    });
+
+    expect(response.status).toBe(400);
   });
 });
