71a22b892c32f1bc93ffb70ec58cea5c7e912daf
Rodrigo <rodrigorael53@gmail.com>
2021-02-24 10:56:04 -0300

    add controller method of creating survey

---
+++ back-end/src/controllers/SurveysController/create.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+
+import { getCustomRepository } from 'typeorm';
+
+import { SurveysRepository } from '../../repositories/SurveysRepository';
+
+export default async function create(request: Request, response: Response) {
+  const { title, description } = request.body;
+
+  const surveysRepository = getCustomRepository(SurveysRepository);
+
+  const survey = surveysRepository.create({
+    title,
+    description
+  });
+
+  await surveysRepository.save(survey);
+
+  return response.status(201).json(survey);
+};
