81cb702667b1b5bacba28664e1a5a7c5a1a2a355
Rodrigo <rodrigorael53@gmail.com>
2021-02-27 01:19:46 -0300

    create the validation in user route with yup

--- back-end/src/controllers/UserController/create.ts
+++ back-end/src/controllers/UserController/create.ts
@@ -3,8 +3,31 @@ import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../../repositories/UsersRepository';
 
+import * as yup from 'yup';
+
 export default async function create(request: Request, response: Response) {
   const { name, email } = request.body;
+
+  // criando o schema da validação
+  const schema = yup.object().shape({
+    // estou falando que tem um name do tipo string e que é obrigatório
+    name: yup.string().required("Nome é obrigatório"),
+    email: yup.string().email().required("Email incorreto")
+  });
+
+  // se as informações que tem dentro do request.body não forem validadas com o 
+  // schema
+  // if (!(await schema.isValid(request.body))) {
+  //   return response.status(400).json({ error: "Validation Failed!" });
+  // }
+
+  // fazendo a validação com o yup
+  try {
+    // o abortEarly: false serve para rodar todas as validações de uma vez
+    await schema.validate(request.body, { abortEarly: false });
+  } catch (error) {
+    return response.status(400).json({ error });
+  };
 
   const usersRepository = getCustomRepository(UsersRepository);
 
